8d255483b76c2c0c685dcece45cd89c05b11da34
Antoine Gros <antoine.gros@map.cnrs.fr>
2024-11-30 12:18:17 +0100

    Hide MongoURI

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
@@ -5,7 +6,7 @@ const app = express()
 
 const Note = require('./models/note')
 
-// Middlewares
+//// MIDDLEWARES (Pre)
 var corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -26,65 +27,8 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 
-// 404 Endpoint
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
-//// DATA
-//// Local
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-
-//// MongoDB
-// const mongoose = require('mongoose')
-
-// console.log(`Connecting to ${process.env.MONGODB_URI}`)
-// const url = process.env.MONGODB_URI
-// mongoose.set('strictQuery',false)
-// mongoose.connect(url)
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// })
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
-
-// const Note = mongoose.model('Note', noteSchema)
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
+//// UTILS
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
@@ -92,7 +36,21 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+
+//// ROUTES
+// /
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// /notes
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -107,22 +65,60 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
+// /notes/:id
 app.get('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
     response.json(note)
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },    
+    { new: true, runValidators: true, context: 'query' }  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+//// MIDDLEWARES (Post)
+// 404 Endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
+
+// ErrorHandling
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {    
+    return response.status(400).json({ error: error.message })  
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+const url = process.env.MONGODB_URI
 
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://helsinki:${password}@helsinki-webdev.zhkhb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=helsinki-webdev`
-
-mongoose.set('strictQuery',false)
+console.log('(.env) connecting to', url)
 
 mongoose.connect(url)
+  .then(result => {
+    console.log('ok, connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
+
+// const password = process.argv[2]
+
+// const url =
+//   `mongodb+srv://helsinki:${password}@helsinki-webdev.zhkhb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=helsinki-webdev`
+
+// mongoose.set('strictQuery',false)
+
+// mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
